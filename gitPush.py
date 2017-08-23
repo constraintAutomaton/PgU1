17ef443d7ab35e4d4d10824ab495128d1c03ce63
bryan.elliott.tam <tambryanelliott@gmail.com>
2017-08-23 14:37:52 -0400

    movement implement in the command module

--- gitPush.py
+++ gitPush.py
@@ -1,8 +1,5 @@
 import os
-import pexpect
 
 message = input('entrez le message du commit: ')
 os.system('cd /home/pi/Documents/Python_project/PgU1_code\ngit add .\ngit commit -m"{}"\ngit push orgin master'.format(message)) 
-git.send('git add .')
-git.send('git commit -m"{}"'.format(message))
-git.send('git push orgin master')
+
