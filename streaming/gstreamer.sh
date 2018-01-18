04b0f3e52c3fd5c46b92eb13c6811cca3f30ed5c
Bryan <tambryanelliott@gmail.com>
2018-01-18 01:49:46 +0000

    small changed in the file location

--- streaming/gstreamer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clear
-raspivid -n -t 0 -rot 270 -w 960 -h 720 -fps 30 -b 6000000 -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=65.94.174.98 port=5000
--- streaming/mjpg.sh
+++
@@ -1,3 +0,0 @@
-cd /usr/src/mjpg-streamer/mjpg-streamer-experimental
-export LD_LIBRARY_PATH=.
-./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 640 -y 480 -fps 20 -ex night"
